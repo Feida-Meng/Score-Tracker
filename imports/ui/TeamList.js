91fc472ab8b1dffdaa585686650dfd5d70717bd4
codonculous <feidameng01@gmail.com>
2017-07-01 19:36:18 -0400

    add conditional rendering for Team class

--- imports/ui/TeamList.js
+++ imports/ui/TeamList.js
@@ -4,13 +4,19 @@ import Team from './Team';
 export default class TeamList extends React.Component {
 
   renderTeams() {
-    return this.props.teams.map ((team) => {
-      return <Team key = {team._id} team = {team}/>;
-    });
+
+    if (this.props.teams.length > 0) {
+      return this.props.teams.map ((team) => {
+        return <Team key = {team._id} team = {team}/>;
+      });
+    } else {
+      return (<p> Add your first team to start tracking! </p> );
+    }
   }
 
   render() {
     return (
+
       <div>
         {this.renderTeams()}
       </div>
