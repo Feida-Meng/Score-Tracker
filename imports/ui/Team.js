7562ac4f1e0cfc59397af6eb0a4eb935ecb7cc24
codonculous <feidameng01@gmail.com>
2017-07-01 17:36:31 -0400

    created render and removeTeam method

--- imports/ui/Team.js
+++ imports/ui/Team.js
@@ -4,11 +4,26 @@ import {Teams} from './../api/teams';
 export default class Team extends React.Component {
 
   addPoints() {
-    Players.update(this.props.player._id,($inc: {score: 1}));
+    Teams.update(this.props.team._id,{$inc: {score: 1} });
   }
 
   removePoint() {
-    Players.update(this.props.player._id,{$inc: {score: -1} });
+    Teams.update(this.props.team._id,{$inc: {score: -1} });
+  }
+
+  removeTeam() {
+    Teams.remove(this.props.team._id);
+  }
+
+  render() {
+    return (
+      <p key = {this.props.team._id}>
+        {this.props.team.name} has {this.props.team.score} point(s).
+        <button onClick = {this.addPoint.bind(this)} > +1 </button>
+        <button onClick = {this.removePoint.bind(this)} > -1 </button>
+        <button onClick = {this.removeTeam.bind(this)} > X </button>
+      </p>
+    );
   }
 
 
