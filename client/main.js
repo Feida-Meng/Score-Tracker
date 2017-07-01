b50fccbcb7ccb6f410694cc6ae779aee116a41f7
codonculous <feidameng01@gmail.com>
2017-07-01 15:09:38 -0400

    created TitleBar component

--- client/main.js
+++ client/main.js
@@ -1,3 +1,12 @@
 import React from  'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
+
+import TitleBar from './../imports/ui/TitleBar';
+
+Meteor.startup( () => {
+  let jsx = (
+    <TitleBar/>
+  )
+  ReactDOM.render(jsx,document.getElementById('app'));
+});
---
+++ imports/ui/TitleBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class TitleBar extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <h1> Score Trackor </h1>
+      </div>
+
+    );
+  }
+}
