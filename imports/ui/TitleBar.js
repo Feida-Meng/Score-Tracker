a6cb8c97c42ee08c4fc57a888c298298617b508e
codonculous <feidameng01@gmail.com>
2017-07-02 11:34:43 -0400

    created and styled subtitle

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -8,6 +8,7 @@ export default class TitleBar extends React.Component {
       <div className = "title-bar">
         <div className = 'wrapper'>
           <h1> Score Trackor</h1>
+          <h4 className = 'title-bar__subtitle'> Created by F.Meng</h4>
         </div>
       </div>
     );
